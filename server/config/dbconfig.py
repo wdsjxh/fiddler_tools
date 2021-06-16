8a6619041604ee07ce3bc9008c8ab2743376b9f8
xuhang <17082762+wdsjxh@users.noreply.github.com>
2021-06-16 15:49:40 +0800

    从config.js中获取后端接口地址

--- server/config/dbconfig.py
+++ server/config/dbconfig.py
@@ -1,3 +1,6 @@
+import re
+
+
 class dbconfig:
     mysqlhost = "127.0.0.1"
     mysqlport = 3306
@@ -5,6 +8,8 @@ class dbconfig:
     mysqlpassword = ""
     mysqldbname = "fiddler_log"
 
+    # my_api_path = "E:\\fiddler_tools\\client\\dist\\config.js"  # 后端接口对应文件路径，源码编译安装时需修改路径
+    my_api_path = "/fiddler_tools/client/dist/config.js"  # 后端接口对应文件路径，源码编译安装时需修改路径
     my_api = "http://127.0.0.1"
 
     def getdbconfig(self):
@@ -17,7 +22,20 @@ class dbconfig:
         return tempconfigdict
 
     def getmyapi(self):
-        return self.my_api
+        try:
+            with open(file=self.my_api_path, mode='r', encoding='utf-8') as api_file:
+                file_content = api_file.readlines()
+                for line in file_content:
+                    if "app_api_url" in line:
+                        print(line)
+                        pattern = re.compile("'(.*)'")
+                        str_re1 = pattern.findall(line)
+                        if str_re1:
+                            return str_re1[0]
+        except IOError as e:
+            print(e)
+            return self.my_api
+
 
 # dbc = dbconfig()
 # print(dbc.getmyapi())
